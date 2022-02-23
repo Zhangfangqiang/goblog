9f20fb7bdc623ca1a72954d819d900e694a8ea21
张舫 <zf18600004319@gmail.com>
2022-02-23 17:24:53 +0800

    '修改文件路径'

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -5,17 +5,21 @@ import (
 	"github.com/gorilla/mux"
 	"goblog/pkg/route"
 	"goblog/routes"
+	"io/fs"
 	"net/http"
 )
 
 // SetupRoute 路由初始化
 func SetupRoute(staticFS embed.FS) *mux.Router {
 	router := mux.NewRouter()
+
 	routes.RegisterWebRoutes(router) //注册路由
-	route.SetRoute(router)           //为了让router 全局统一
+	route.SetRoute(router)           //pkg  为了让router 全局统一
 
 	// 静态资源
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("resources/assets")))
+	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	sub, _ := fs.Sub(staticFS, "resources/assets")
+	router.PathPrefix("/").Handler(http.FileServer(http.FS(sub)))
 
 	return router
 }
